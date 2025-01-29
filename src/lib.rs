1cd8c4756b81c9a28a0357fa29d66118b0a49c07
dariusbakunas <bakunas@gmail.com>
2025-01-28 20:49:35 -0500

    refactor: cleanup unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,6 @@
 pub mod cli;
 pub mod constants;
 pub mod inventory;
+pub mod playbook;
+pub mod ssh;
 pub mod vault;
-
-use crate::inventory::manager;
-use anyhow::Result;
-use clap::Parser;
-use cli::Cli;
-
-pub fn run() -> Result<()> {
-    let cli = Cli::parse();
-    let inventory = cli.inventory.as_ref();
-
-    let mut manager = manager::InventoryManager::new();
-    manager.parse_sources(inventory)?;
-
-    if cli.list_hosts {
-        let mut hosts: Vec<String> = manager
-            .filter_hosts(cli.pattern.as_str(), cli.limit.as_deref())?
-            .iter()
-            .map(|h| h.name.to_string())
-            .collect();
-        hosts.sort();
-        for host in hosts {
-            println!("{host}");
-        }
-        Ok(())
-    } else {
-        todo!();
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-mod modules;
-mod playbook;
-mod ssh;
-
 use anyhow::Result;
+use clap::Parser;
+use cogrs::cli::Cli;
+use cogrs::inventory::manager;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -10,5 +9,28 @@ async fn main() -> Result<()> {
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
 
-    cogrs::run()
+    run()
+}
+
+fn run() -> Result<()> {
+    let cli = Cli::parse();
+    let inventory = cli.inventory.as_ref();
+
+    let mut manager = manager::InventoryManager::new();
+    manager.parse_sources(inventory)?;
+
+    if cli.list_hosts {
+        let mut hosts: Vec<String> = manager
+            .filter_hosts(cli.pattern.as_str(), cli.limit.as_deref())?
+            .iter()
+            .map(|h| h.name.to_string())
+            .collect();
+        hosts.sort();
+        for host in hosts {
+            println!("{host}");
+        }
+        Ok(())
+    } else {
+        todo!();
+    }
 }
