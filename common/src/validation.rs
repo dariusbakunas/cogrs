ce4df5284998ef82fe6bc7345923f8286e783013
dariusbakunas <bakunas@gmail.com>
2025-02-01 11:39:10 -0500

    test: add validation tests

--- common/src/validation.rs
+++ common/src/validation.rs
@@ -19,3 +19,48 @@ pub fn validate_input(schema: &str, input: &Value) -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::validate_input;
+
+    #[test]
+    fn test_valid_input() {
+        let schema = r#"{
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"}
+            },
+            "required": ["name"]
+        }"#;
+        let input = serde_json::from_str(r#"{"name": "Cogrs"}"#).unwrap();
+        assert!(validate_input(schema, &input).is_ok());
+    }
+
+    #[test]
+    fn test_invalid_input() {
+        let schema = r#"{
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"}
+            },
+            "required": ["name"]
+        }"#;
+        let input = serde_json::from_str(r#"{"age": 30}"#).unwrap();
+        assert!(validate_input(schema, &input).is_err());
+    }
+
+    #[test]
+    fn test_unknown_property() {
+        let schema = r#"{
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"}
+            },
+            "additionalProperties": false,
+            "required": ["name"]
+        }"#;
+        let input = serde_json::from_str(r#"{"name": "Cogrs", "test": "invalid"}"#).unwrap();
+        assert!(validate_input(schema, &input).is_err());
+    }
+}
